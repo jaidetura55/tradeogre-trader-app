b0146d4bade027bd3f92826c8726b03e9cd69ae7
robertokbr <robertojuniordev@gmail.com>
2021-06-23 17:15:09 -0300

    Add CoinMarketCap API Provider

---
+++ src/providers/CoinMarketCap.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance } from "axios";
+import coinmarketcap from "../configs/coinmarketcap";
+import CryptoCoin from "../definitions/ICryptoCoin";
+
+interface ICryptocurrencyReturn {
+  status: string;
+  data: Array<CryptoCoin>;
+}
+
+class CoinMarketCap {
+  private client: AxiosInstance;
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: coinmarketcap.endpoint,
+      headers: {
+        'X-CMC_PRO_API_KEY': coinmarketcap.key,
+      },
+    });
+  }
+
+  public async getCryptoCurrency(code: string) {
+    const { data } = await this.client.get<ICryptocurrencyReturn>('cryptocurrency/listings/latest');
+  
+    const asset = data.data.find(({ symbol }) => symbol === code)
+
+    return asset
+  }
+}
+
+export default CoinMarketCap;
