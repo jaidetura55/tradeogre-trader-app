ea6251a5c11fd5be604c4e776da4a31c214041b3
robertokbr <robertojuniordev@gmail.com>
2021-06-23 21:41:41 -0300

    Add  start operation by env variable logic

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,53 @@
 import App from "./App";
 
+type IOperations = 'buy' | 'sell';
+
 const app = new App();
 
-const asset_code = String(process.env.ASSET_CODE);
-const price = Number(process.env.ASSET_PRICE);
-const quantity = Number(process.env.ASSET_QUANTITY);
+const operation = process.env.OPERATION as IOperations;
 
-console.info(
-  `Starting app with parans: 
-    asset ${asset_code}, 
-    price ${price}, 
-    quantity ${quantity}
-  `
-);
+const operation_functions = {
+  sell: async () => {
+    const asset_code = String(process.env.ASSET_SELL_CODE);
+    const price = Number(process.env.ASSET_SELL_PRICE);
+    const quantity = Number(process.env.ASSET_SELL_QUANTITY);
+  
+    console.info(
+      `Starting app with parans: 
+        asset ${asset_code}, 
+        price ${price}, 
+        quantity ${quantity}
+      `
+    );
+  
+    app.sellOrder({
+      asset_code,
+      price,
+      quantity,
+    });
+  },
+  buy: async () => {
+    const asset_code = String(process.env.ASSET_BUY_CODE);
+    const price = Number(process.env.ASSET_BUY_PRICE);
+    const quantity = Number(process.env.ASSET_BUY_QUANTITY);
+  
+    console.info(
+      `Starting app with parans: 
+        asset ${asset_code}, 
+        price ${price}, 
+        quantity ${quantity}
+      `
+    );
+  
+    app.buyOrder({
+      asset_code,
+      price,
+      quantity,
+    });
+  }
+}
 
-app.sellOrder({
-  asset_code,
-  price,
-  quantity,
-});
+
+const selectd_operation_function = operation_functions[operation];
+
+selectd_operation_function();
