65c19df06cc16e0cc49d0240b645d2f2965de2b5
robertokbr <robertojuniordev@gmail.com>
2021-06-26 18:42:53 -0300

    Implement application

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,23 @@
+import { requestOperationData } from "./actions/getOperationData";
 import App from "./App";
 
-type IOperations = 'buy' | 'sell';
+type SimpleObject = {
+  [key: string]: 'buyOrder' | 'sellOrder';
+}
+
+const { amount, market, operation, price } = requestOperationData();
 
 const app = new App();
 
-const operation = process.env.OPERATION as IOperations;
+const operation_method = {
+  buy: 'buyOrder',
+  sell: 'sellOrder',
+} as SimpleObject;
 
-const operation_functions = {
-  sell: async () => {
-    const asset_code = String(process.env.ASSET_SELL_CODE);
-    const price = Number(process.env.ASSET_SELL_PRICE);
-    const quantity = Number(process.env.ASSET_SELL_QUANTITY);
-  
-    console.info(
-      `Starting app with parans: 
-        asset ${asset_code}, 
-        price ${price}, 
-        quantity ${quantity}
-      `
-    );
-  
-    app.sellOrder({
-      asset_code,
-      price,
-      quantity,
-    });
-  },
-  buy: async () => {
-    const asset_code = String(process.env.ASSET_BUY_CODE);
-    const price = Number(process.env.ASSET_BUY_PRICE);
-    const quantity = Number(process.env.ASSET_BUY_QUANTITY);
-  
-    console.info(
-      `Starting app with parans: 
-        asset ${asset_code}, 
-        price ${price}, 
-        quantity ${quantity}
-      `
-    );
-  
-    app.buyOrder({
-      asset_code,
-      price,
-      quantity,
-    });
-  }
-}
+const method = operation_method[operation];
 
-
-const selectd_operation_function = operation_functions[operation];
-
-selectd_operation_function();
+app[method]({
+  asset_code: market,
+  price: + price,
+  quantity: + amount,
+});
