b39ef5331c5f0c07d85703bd2ac55ba8c3e8661e
robertokbr <robertojuniordev@gmail.com>
2021-07-02 20:27:49 -0300

    Add findFallingCryptosOfTheWeek method

--- src/providers/CoinMarketCap.ts
+++ src/providers/CoinMarketCap.ts
@@ -19,10 +19,20 @@ class CoinMarketCap {
     });
   }
 
-  public async getCryptoCurrency(code: string) {
+  public async findCryptoByCode(code: string) {
     const { data } = await this.client.get<ICryptocurrencyReturn>('cryptocurrency/listings/latest');
   
     const asset = data.data.find(({ symbol }) => symbol === code)
+
+    return asset
+  }
+
+  public async findFallingCryptosOfTheWeek(percentage: number) {
+    const { data } = await this.client.get<ICryptocurrencyReturn>('cryptocurrency/listings/latest');
+  
+    const asset = data.data.filter(
+      ({ quote: { USD } }) => USD.percent_change_7d <= percentage,
+    );
 
     return asset
   }
