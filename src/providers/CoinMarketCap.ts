abdc851a6f932f5795f85b7f70dc77f15f833410
robertokbr <robertojuniordev@gmail.com>
2021-07-03 16:06:26 -0300

    Refactor class and add findCryptoByMovement method

--- src/providers/CoinMarketCap.ts
+++ src/providers/CoinMarketCap.ts
@@ -1,10 +1,22 @@
 import axios, { AxiosInstance } from "axios";
 import coinmarketcap from "../configs/coinmarketcap";
-import { ICryptoCoin } from "../models/ICryptoCoin";
+import AppError from "../error/AppError";
+import { ICMCCrypto } from "../models/ICMCCrypto";
+
+type IDateRange = 
+`percent_change_24h`
+| `percent_change_7d`
+| `percent_change_30d`
 
 interface ICryptocurrencyReturn {
   status: string;
-  data: Array<ICryptoCoin>;
+  data: Array<ICMCCrypto>;
+}
+
+export interface IFindCryptoByMarketMovementDTO {
+  market_movement: 'down' | 'up'
+  date_range: `day` | `week` | `month`
+  percent: number
 }
 
 class CoinMarketCap {
@@ -27,11 +39,36 @@ class CoinMarketCap {
     return asset
   }
 
-  public async findFallingCryptosOfTheWeek(percentage: number) {
-    const { data } = await this.client.get<ICryptocurrencyReturn>('cryptocurrency/listings/latest');
-  
+  public async findCryptoByMovement({ market_movement ,percent, date_range }: IFindCryptoByMarketMovementDTO) {  
+    const date_range_correspondence = {
+      day: `percent_change_24h`,
+      week: `percent_change_7d`,
+      month: `percent_change_30d`
+    }
+
+    const percent_change_range = date_range_correspondence[date_range] as IDateRange
+
+    if (!percent_change_range) {
+      throw new AppError({
+        description: 'Wrong request params',
+        message: 'You must provide the right params',
+        documentation: {
+          market_movement: ['down', 'up'],
+          date_range: [`day` , `week` , `month`],
+          percent: "number"
+        }
+      });
+    }
+
+    const { data } = await this.client.get<ICryptocurrencyReturn>(
+      'cryptocurrency/listings/latest'
+    );
+
     const asset = data.data.filter(
-      ({ quote: { USD } }) => USD.percent_change_7d <= percentage,
+      ({ quote: { USD } }) => 
+        market_movement === 'down' 
+        ? USD[percent_change_range] <= percent 
+        : USD[percent_change_range] >= percent
     );
 
     return asset
